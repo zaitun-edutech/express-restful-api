71d580ab5e967a8d9985a18c0a0bb4bb1a3d88af
zaitun-edutech <zaitunedutech@gmail.com>
2022-05-08 02:46:15 -0700

    menambahkan cors policy

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/test",
+  //   url: "mongodb://localhost:27017/test",
+  url: "mongodb+srv://awal:Buraneta@cluster0.9uhew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
